41a9c1e25c93da00d243fb2db15f9dfd01a41eb2
thalber <73020111+thalber@users.noreply.github.com>
2023-01-13 20:31:01 +0400

    Add machinery

---
+++ src/Modules/Machinery/OscillationParams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegionKit.Machinery
+{
+    public struct OscillationParams
+    {
+        public readonly float amp;
+        public readonly float frq;
+        public readonly float phase;
+        public Func<float, float> oscm => _oscm ?? UnityEngine.Mathf.Sin;
+        private Func<float, float> _oscm;
+
+        public OscillationParams(float amp, float frq, float phase, Func<float, float> oscm)
+        {
+            this.amp = amp;
+            this.frq = frq;
+            this.phase = phase;
+            this._oscm = oscm;
+        }
+
+        public OscillationParams Deviate(in OscillationParams fluke)
+        {
+            return new OscillationParams(ClampedFloatDeviation(this.amp, fluke.amp),
+                ClampedFloatDeviation(this.frq, fluke.frq),
+                ClampedFloatDeviation(phase, fluke.phase), (UnityEngine.Random.value < 0.5) ? this.oscm : fluke.oscm);
+        }
+    }
+}
---
+++ src/Modules/Machinery/SimplePiston.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using static RWCustom.Custom;
+using static UnityEngine.Mathf;
+
+namespace RegionKit.Machinery
+{
+    public class SimplePiston : UpdatableAndDeletable, IDrawable
+    {
+        public SimplePiston(Room rm, PlacedObject pobj) : this(rm, pobj, null) { }
+        public SimplePiston(Room rm, PlacedObject pobj, PistonData mdt = null)
+        {
+            PO = pobj;
+            this._assignedMData = mdt;
+            plog.LogDebug($"({rm.abstractRoom.name}): Created simplePiston" + mdt == null ? "." : "as a part of an array.");
+        }
+        public override void Update(bool eu)
+        {
+            base.Update(eu);
+            oldPos = currentPos;
+            _lt += room.ElectricPower;
+            currentPos = originPoint + DegToVec(effRot) * Shift;
+        }
+
+        internal PistonData mData
+        {
+            get
+            {
+                var r = _assignedMData ?? PO?.data as PistonData;
+                if (r == null) { _bpd = _bpd ?? new PistonData(null); return _bpd; }
+                return r;
+            }
+        }
+        private PistonData _bpd;
+        private readonly PistonData _assignedMData;
+
+        internal readonly PlacedObject PO;
+        private double _lt = 0f;
+        internal Vector2 originPoint => PO?.pos ?? _assignedMData?.forcePos ?? default;
+        internal float effRot => mData.align ? ((int)mData.rotation / 45 * 45) : mData.rotation;
+        internal float Shift
+        {
+            get
+            {
+                var res = mData.amplitude;
+                Func<double, double> chosenFunc;
+                switch (mData.opmode)
+                {
+                    default:
+                    case OperationMode.Sinal:
+                        chosenFunc = Math.Sin;
+                        break;
+                    case OperationMode.Cosinal:
+                        chosenFunc = Math.Cos;
+                        break;
+                }
+                res *= (float)chosenFunc((_lt + mData.phase) * mData.frequency);
+                return res;
+            }
+        }
+        internal Vector2 currentPos;
+        internal Vector2 oldPos;
+
+        internal MachineryCustomizer _mc;
+        
+        internal void GrabMC()
+        {
+            _mc = _mc 
+                ?? room.roomSettings.placedObjects.FirstOrDefault(
+                    x => x.data is MachineryCustomizer nmc && nmc.GetValue<MachineryID>("amID") == MachineryID.Piston && (x.pos - this.PO.pos).sqrMagnitude <= nmc.GetValue<Vector2>("radius").sqrMagnitude)?.data as MachineryCustomizer 
+                ?? new MachineryCustomizer(null);
+        }
+
+        #region irawable things
+        public void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+        {
+            GrabMC();
+            sLeaser.sprites = new FSprite[1];
+            sLeaser.sprites[0] = new FSprite("pixel");
+            this.AddToContainer(sLeaser, rCam, null);
+        }
+
+        public void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            var pos = Vector2.Lerp(oldPos, currentPos, timeStacker);
+            _mc.BringToKin(sLeaser.sprites[0]);
+            sLeaser.sprites[0].rotation = effRot + _mc.addRot;
+            sLeaser.sprites[0].x = pos.x - camPos.x;
+            sLeaser.sprites[0].y = pos.y - camPos.y;
+        }
+
+        public void ApplyPalette(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
+        {
+
+        }
+
+        public void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
+        {
+            foreach (var fs in sLeaser.sprites) fs.RemoveFromContainer();
+            try { (newContatiner ?? rCam.ReturnFContainer(_mc.ContainerName)).AddChild(sLeaser.sprites[0]); }
+            catch { rCam.ReturnFContainer("Items").AddChild(sLeaser.sprites[0]); }
+            
+        }
+        #endregion
+    }
+
+}

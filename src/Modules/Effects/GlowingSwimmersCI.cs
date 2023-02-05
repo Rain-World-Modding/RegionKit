1d12ab222f9d3438660c54eb0cf0c78cd7b4c4f2
Fyëryn <67385290+M4rbleL1ne@users.noreply.github.com>
2023-02-05 21:17:46 +0100

    Updated GlowingSwimmers

--- src/Modules/Effects/GlowingSwimmersCI.cs
+++ src/Modules/Effects/GlowingSwimmersCI.cs
@@ -1,55 +1,50 @@
-﻿using System;
-using RWCustom;
-using UnityEngine;
-using static RWCustom.Custom;
-using Random = UnityEngine.Random;
+﻿namespace RegionKit.Modules.Effects;
 
 /// <summary>
-/// By LB Gamer/M4rbleL1ne
+/// By LB/M4rbleL1ne
 /// </summary>
-
-namespace RegionKit.Modules.Effects;
-
-public class GlowingSwimmersCI
+internal static class GlowingSwimmersCI
 {
+	/// <summary>
+	/// Enums
+	/// </summary>
 	public static class Enums_GlowingSwimmers
 	{
+		/// <summary>
+		/// Effect type
+		/// </summary>
 		public static RoomSettings.RoomEffect.Type GlowingSwimmers = new(nameof(GlowingSwimmers), true);
+		/// <summary>
+		/// Insect type
+		/// </summary>
 		public static CosmeticInsect.Type GlowingSwimmerInsect = new(nameof(GlowingSwimmerInsect), true);
 	}
 
-	public static void Apply()
+	internal static void Apply()
 	{
 		_CommonHooks.PostRoomLoad += RoomPostLoad;
-		On.InsectCoordinator.SpeciesDensity_Type_1 += (orig, type) => type == Enums_GlowingSwimmers.GlowingSwimmerInsect ? .8f : orig(type);
-		On.InsectCoordinator.RoomEffectToInsectType += (orig, type) => type == Enums_GlowingSwimmers.GlowingSwimmers ? Enums_GlowingSwimmers.GlowingSwimmerInsect : orig(type);
-		On.InsectCoordinator.TileLegalForInsect += (orig, type, room, testPos) => type == Enums_GlowingSwimmers.GlowingSwimmerInsect ? room.GetTile(testPos).DeepWater : orig(type, room, testPos);
-		On.InsectCoordinator.EffectSpawnChanceForInsect += (orig, type, room, testPos, effectAmount) => type == Enums_GlowingSwimmers.GlowingSwimmerInsect || orig(type, room, testPos, effectAmount);
-		On.InsectCoordinator.CreateInsect += (orig, self, type, pos, swarm) =>
-			{
-				if (!InsectCoordinator.TileLegalForInsect(type, self.room, pos) || self.room.world.rainCycle.TimeUntilRain < Random.Range(1200, 1600))
-					return;
-				if (type == Enums_GlowingSwimmers.GlowingSwimmerInsect)
-				{
-					var cosmeticInsect = new GlowingSwimmer(self.room, pos);
-					self.allInsects.Add(cosmeticInsect);
-					if (swarm != null)
-					{
-						swarm.members.Add(cosmeticInsect);
-						cosmeticInsect.mySwarm = swarm;
-					}
-					self.room.AddObject(cosmeticInsect);
-				}
-				else
-					orig(self, type, pos, swarm);
-			};
+		On.InsectCoordinator.SpeciesDensity_Type_1 += InsectCoordinatorSpeciesDensityInsectType;
+		On.InsectCoordinator.RoomEffectToInsectType += InsectCoordinatorRoomEffectToInsectType;
+		On.InsectCoordinator.TileLegalForInsect += InsectCoordinatorTileLegalForInsect;
+		On.InsectCoordinator.EffectSpawnChanceForInsect += InsectCoordinatorEffectSpawnChanceForInsect;
+		On.InsectCoordinator.CreateInsect += InsectCoordinatorCreateInsect;
+	}
+
+	internal static void Undo()
+	{
+		_CommonHooks.PostRoomLoad -= RoomPostLoad;
+		On.InsectCoordinator.SpeciesDensity_Type_1 -= InsectCoordinatorSpeciesDensityInsectType;
+		On.InsectCoordinator.RoomEffectToInsectType -= InsectCoordinatorRoomEffectToInsectType;
+		On.InsectCoordinator.TileLegalForInsect -= InsectCoordinatorTileLegalForInsect;
+		On.InsectCoordinator.EffectSpawnChanceForInsect -= InsectCoordinatorEffectSpawnChanceForInsect;
+		On.InsectCoordinator.CreateInsect -= InsectCoordinatorCreateInsect;
 	}
 
 	private static void RoomPostLoad(Room self)
 	{
 		for (var i = 0; i < self.roomSettings.effects.Count; i++)
 		{
-			var effect = self.roomSettings.effects[i];
+			RoomSettings.RoomEffect effect = self.roomSettings.effects[i];
 			if (effect.type == Enums_GlowingSwimmers.GlowingSwimmers)
 			{
 				if (self.insectCoordinator is null)
@@ -61,8 +56,50 @@ public class GlowingSwimmersCI
 			}
 		}
 	}
+
+	private static float InsectCoordinatorSpeciesDensityInsectType(On.InsectCoordinator.orig_SpeciesDensity_Type_1 orig, CosmeticInsect.Type type)
+	{
+		return type == Enums_GlowingSwimmers.GlowingSwimmerInsect ? .8f : orig(type);
+	}
+
+	private static CosmeticInsect.Type InsectCoordinatorRoomEffectToInsectType(On.InsectCoordinator.orig_RoomEffectToInsectType orig, RoomSettings.RoomEffect.Type type)
+	{
+		return type == Enums_GlowingSwimmers.GlowingSwimmers ? Enums_GlowingSwimmers.GlowingSwimmerInsect : orig(type);
+	}
+
+	private static bool InsectCoordinatorTileLegalForInsect(On.InsectCoordinator.orig_TileLegalForInsect orig, CosmeticInsect.Type type, Room room, Vector2 testPos)
+	{
+		return type == Enums_GlowingSwimmers.GlowingSwimmerInsect ? room.GetTile(testPos).DeepWater : orig(type, room, testPos);
+	}
+
+	private static bool InsectCoordinatorEffectSpawnChanceForInsect(On.InsectCoordinator.orig_EffectSpawnChanceForInsect orig, CosmeticInsect.Type type, Room room, Vector2 testPos, float effectAmount)
+	{
+		return type == Enums_GlowingSwimmers.GlowingSwimmerInsect || orig(type, room, testPos, effectAmount);
+	}
+
+	private static void InsectCoordinatorCreateInsect(On.InsectCoordinator.orig_CreateInsect orig, InsectCoordinator self, CosmeticInsect.Type type, Vector2 pos, InsectCoordinator.Swarm swarm)
+	{
+		if (type == Enums_GlowingSwimmers.GlowingSwimmerInsect)
+		{
+			if (!InsectCoordinator.TileLegalForInsect(type, self.room, pos) || self.room.world.rainCycle.TimeUntilRain < RNG.Range(1200, 1600))
+				return;
+			var cosmeticInsect = new GlowingSwimmer(self.room, pos);
+			self.allInsects.Add(cosmeticInsect);
+			if (swarm != null)
+			{
+				swarm.members.Add(cosmeticInsect);
+				cosmeticInsect.mySwarm = swarm;
+			}
+			self.room.AddObject(cosmeticInsect);
+		}
+		else
+			orig(self, type, pos, swarm);
+	}
 }
 
+/// <summary>
+/// Glowing swimmer
+/// </summary>
 public class GlowingSwimmer : CosmeticInsect
 {
 	private float _stressed;
@@ -71,17 +108,24 @@ public class GlowingSwimmer : CosmeticInsect
 	private Vector2 _swimDir;
 	private float _breath;
 	private float _lastBreath;
-	private Vector2[,] _segments;
+	private readonly Vector2[,] _segments;
 	private LightSource? _lightSource;
 
+	/// <summary>
+	/// Glowing swimmer
+	/// </summary>
 	public GlowingSwimmer(Room room, Vector2 pos) : base(room, pos, GlowingSwimmersCI.Enums_GlowingSwimmers.GlowingSwimmerInsect)
 	{
 		creatureAvoider = new(this, 10, 300f, .3f);
-		_breath = Random.value;
+		_breath = RNG.value;
 		_segments = new Vector2[2, 2];
 		Reset(pos);
 	}
 
+	/// <summary>
+	/// Update
+	/// </summary>
+	/// <param name="eu"></param>
 	public override void Update(bool eu)
 	{
 		_lastRot = _rot;
@@ -98,9 +142,9 @@ public class GlowingSwimmer : CosmeticInsect
 				_segments[i, 1] *= .8f;
 			else
 				_segments[i, 1].y -= .9f;
-			if (i is 0)
+			if (i == 0)
 			{
-				var vector = DirVec(_segments[i, 0], pos);
+				Vector2 vector = DirVec(_segments[i, 0], pos);
 				var num = Vector2.Distance(_segments[i, 0], pos);
 				pos += vector * (4f - num) * .5f;
 				vel += vector * (4f - num) * .5f;
@@ -110,7 +154,7 @@ public class GlowingSwimmer : CosmeticInsect
 			}
 			else
 			{
-				var vector2 = DirVec(_segments[i, 0], _segments[i - 1, 0]);
+				Vector2 vector2 = DirVec(_segments[i, 0], _segments[i - 1, 0]);
 				var num2 = Vector2.Distance(_segments[i, 0], _segments[i - 1, 0]);
 				_segments[i - 1, 0] += vector2 * (4f - num2) * .5f;
 				_segments[i - 1, 1] += vector2 * (4f - num2) * .5f;
@@ -119,41 +163,45 @@ public class GlowingSwimmer : CosmeticInsect
 				_segments[i - 1, 1] += vector2 * Mathf.Lerp(.8f, 1.2f, _stressed);
 			}
 		}
-		if (room != null)
+		if (room is Room rm)
 		{
-			if (_lightSource != null)
+			if (_lightSource is LightSource ls)
 			{
-				_lightSource.stayAlive = true;
-				_lightSource.setPos = pos;
-				_lightSource.color = room.game.cameras[0].currentPalette.waterColor1;
-				_lightSource.affectedByPaletteDarkness = 0f;
-				if (_lightSource.slatedForDeletetion || !submerged)
+				ls.stayAlive = true;
+				ls.setPos = pos;
+				ls.color = rm.game.cameras[0].currentPalette.waterColor1;
+				ls.affectedByPaletteDarkness = 0f;
+				if (ls.slatedForDeletetion || !submerged)
 					_lightSource = null;
 			}
 			else if (submerged)
-			{
-				_lightSource = new(pos, false, room.game.cameras[0].currentPalette.waterColor1, this)
+				room.AddObject(_lightSource = new(pos, false, rm.game.cameras[0].currentPalette.waterColor1, this)
 				{
 					requireUpKeep = true,
 					setRad = 200f,
 					setAlpha = 1f,
 					affectedByPaletteDarkness = 0f
-				};
-				room.AddObject(_lightSource);
-			}
+				});
 		}
 	}
 
+	/// <summary>
+	/// Reset
+	/// </summary>
+	/// <param name="resetPos"></param>
 	public override void Reset(Vector2 resetPos)
 	{
 		base.Reset(resetPos);
 		for (var i = 0; i < _segments.GetLength(0); i++)
 		{
 			_segments[i, 0] = resetPos + RNV();
-			_segments[i, 1] = RNV() * Random.value;
+			_segments[i, 1] = RNV() * RNG.value;
 		}
 	}
 
+	/// <summary>
+	/// Act
+	/// </summary>
 	public override void Act()
 	{
 		base.Act();
@@ -165,31 +213,40 @@ public class GlowingSwimmer : CosmeticInsect
 			_stressed = LerpAndTick(_stressed, num, .02f, .005f);
 		if (submerged)
 		{
-			_swimDir += RNV() * Random.value * .5f;
+			_swimDir += RNV() * RNG.value * .5f;
 			if (wantToBurrow)
 				_swimDir.y -= .5f;
 			if (pos.x < 0f)
 				_swimDir.x += 1f;
-			else if (pos.x > room.PixelWidth)
+			else if (pos.x > room?.PixelWidth)
 				_swimDir.x -= 1f;
 			if (pos.y < 0f)
 				_swimDir.y += 1f;
-			if (creatureAvoider.currentWorstCrit != null)
-				_swimDir -= DirVec(pos, creatureAvoider.currentWorstCrit.DangerPos) * creatureAvoider.FleeSpeed;
-			if (room.water)
-				_swimDir = Vector3.Slerp(_swimDir, new(0f, -1f), Mathf.InverseLerp(room.FloatWaterLevel(pos.x) - 100f, room.FloatWaterLevel(pos.x), pos.y) * .5f);
+			if (creatureAvoider.currentWorstCrit is Creature c)
+				_swimDir -= DirVec(pos, c.DangerPos) * creatureAvoider.FleeSpeed;
+			if (room is Room rm && rm.water)
+				_swimDir = Vector3.Slerp(_swimDir, new(0f, -1f), Mathf.InverseLerp(rm.FloatWaterLevel(pos.x) - 100f, rm.FloatWaterLevel(pos.x), pos.y) * .5f);
 			_swimDir.Normalize();
-			vel += (_swimDir * Mathf.Lerp(.8f, 1.1f, _stressed) + RNV() * Random.value * .1f) / 1.2f;
+			vel += (_swimDir * Mathf.Lerp(.8f, 1.1f, _stressed) + RNV() * RNG.value * .1f) / 1.2f;
 		}
 		_rot = Vector3.Slerp(_rot, (-vel - _swimDir).normalized, .2f);
 	}
 
+	/// <summary>
+	/// Wall collision
+	/// </summary>
+	/// <param name="dir"></param>
+	/// <param name="first"></param>
 	public override void WallCollision(IntVector2 dir, bool first)
 	{
-		_swimDir -= RNV() * Random.value + dir.ToVector2();
+		_swimDir -= RNV() * RNG.value + dir.ToVector2();
 		_swimDir.Normalize();
 	}
 
+	/// <summary>
+	/// Emerge from ground
+	/// </summary>
+	/// <param name="emergePos"></param>
 	public override void EmergeFromGround(Vector2 emergePos)
 	{
 		base.EmergeFromGround(emergePos);
@@ -197,33 +254,44 @@ public class GlowingSwimmer : CosmeticInsect
 		_swimDir = new(0f, 1f);
 	}
 
-	private int _LegSprite(int segment, int leg) => segment * 2 + leg;
+	private int LegSprite(int segment, int leg) => segment * 2 + leg;
 
-	private int _SegmentSprite(int segment, int part) => (_segments.GetLength(0) + 1) * 2 + segment * 2 + part;
+	private int SegmentSprite(int segment, int part) => (_segments.GetLength(0) + 1) * 2 + segment * 2 + part;
 
+	/// <summary>
+	/// Initiate sprites
+	/// </summary>
+	/// <param name="sLeaser"></param>
+	/// <param name="rCam"></param>
 	public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
 	{
 		base.InitiateSprites(sLeaser, rCam);
 		sLeaser.sprites = new FSprite[(_segments.GetLength(0) + 1) * 4];
 		for (var i = 0; i < _segments.GetLength(0) + 1; i++)
 		{
-			sLeaser.sprites[_SegmentSprite(i, 0)] = new("Circle20") { anchorY = .3f };
-			sLeaser.sprites[_SegmentSprite(i, 1)] = new("Circle20") { anchorY = .4f };
-			sLeaser.sprites[_LegSprite(i, 0)] = new("pixel") { anchorY = 0f };
-			sLeaser.sprites[_LegSprite(i, 1)] = new("pixel") { anchorY = 0f };
+			sLeaser.sprites[SegmentSprite(i, 0)] = new("Circle20") { anchorY = .3f };
+			sLeaser.sprites[SegmentSprite(i, 1)] = new("Circle20") { anchorY = .4f };
+			sLeaser.sprites[LegSprite(i, 0)] = new("pixel") { anchorY = 0f };
+			sLeaser.sprites[LegSprite(i, 1)] = new("pixel") { anchorY = 0f };
 		}
 		AddToContainer(sLeaser, rCam, null);
 	}
 
+	/// <summary>
+	/// Draw sprites
+	/// </summary>
+	/// <param name="sLeaser"></param>
+	/// <param name="rCam"></param>
+	/// <param name="timeStacker"></param>
+	/// <param name="camPos"></param>
 	public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
 	{
 		base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
 		var num = Mathf.Lerp(lastInGround, inGround, timeStacker);
 		for (var i = 0; i < _segments.GetLength(0) + 1; i++)
 		{
-			var t = .5f + .5f * Mathf.Sin((Mathf.Lerp(_lastBreath, _breath, timeStacker) + .1f * i) * 7f * (float)Math.PI);
-			var p = (i != 0) ? _segments[i - 1, 0] : Vector2.Lerp(lastPos, pos, timeStacker);
-			var v = i switch
+			var t = .5f + .5f * Mathf.Sin((Mathf.Lerp(_lastBreath, _breath, timeStacker) + .1f * i) * 7f * Mathf.PI);
+			Vector2 p = (i != 0) ? _segments[i - 1, 0] : Vector2.Lerp(lastPos, pos, timeStacker), v = i switch
 			{
 				0 => (Vector2)(-Vector3.Slerp(_lastRot, _rot, timeStacker)),
 				1 => DirVec(p, Vector2.Lerp(lastPos, pos, timeStacker)),
@@ -233,13 +301,12 @@ public class GlowingSwimmer : CosmeticInsect
 			var num3 = LerpMap(i, 0f, _segments.GetLength(0), 1f, .5f, 1.2f) * (1f - num);
 			for (var j = 0; j < 2; j++)
 			{
-				var seg = sLeaser.sprites[_SegmentSprite(i, j)];
+				FSprite seg = sLeaser.sprites[SegmentSprite(i, j)], leg = sLeaser.sprites[LegSprite(i, j)];
 				seg.x = p.x - camPos.x;
 				seg.y = p.y - camPos.y;
 				seg.rotation = VecToDeg(v);
 				seg.scaleX = 4f * num3 * (1f - num) / 15f;
 				seg.scaleY = 6.5f * num3 * (1f - num) / 15f;
-				var leg = sLeaser.sprites[_LegSprite(i, j)];
 				leg.x = p.x - PerpendicularVector(v).x * 2f * num3 * ((j != 0) ? 1f : -1f) - camPos.x;
 				leg.y = p.y - PerpendicularVector(v).y * 2f * num3 * ((j != 0) ? 1f : -1f) - camPos.y;
 				leg.rotation = VecToDeg(v) + (Mathf.Lerp(-20f, 70f, t) + LerpMap(i, 0f, _segments.GetLength(0), 70f, 140f)) * ((j != 0) ? 1f : -1f);
@@ -248,13 +315,19 @@ public class GlowingSwimmer : CosmeticInsect
 		}
 	}
 
+	/// <summary>
+	/// Apply palette
+	/// </summary>
+	/// <param name="sLeaser"></param>
+	/// <param name="rCam"></param>
+	/// <param name="palette"></param>
 	public override void ApplyPalette(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
 	{
 		base.ApplyPalette(sLeaser, rCam, palette);
 		for (var i = 0; i < _segments.GetLength(0) + 1; i++)
 		{
-			sLeaser.sprites[_SegmentSprite(i, 0)].color = palette.blackColor;
-			sLeaser.sprites[_SegmentSprite(i, 1)].color = palette.blackColor;
+			sLeaser.sprites[SegmentSprite(i, 0)].color = palette.blackColor;
+			sLeaser.sprites[SegmentSprite(i, 1)].color = palette.blackColor;
 		}
 	}
 }

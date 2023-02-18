61c87dbb67e224c3b7ef5495adb347bb4b7e034f
Bro748 <103339131+Bro748@users.noreply.github.com>
2023-02-18 15:13:05 -0700

    Removing namespace indentation

--- src/Modules/DevUIMisc/BackgroundBuilder.cs
+++ src/Modules/DevUIMisc/BackgroundBuilder.cs
@@ -9,45 +9,45 @@ using System.Threading.Tasks;
 using DevInterface;
 using Random = UnityEngine.Random;
 
-namespace RegionKit.Modules.DevUIMisc
+namespace RegionKit.Modules.DevUIMisc;
+
+internal static class BackgroundBuilder
 {
-	internal static class BackgroundBuilder
+	public static void Apply()
 	{
-		public static void Apply()
+		On.DevInterface.DevUI.ctor += DevUI_ctor;
+		On.DevInterface.DevUI.SwitchPage += DevUI_SwitchPage;
+	}
+
+
+	public static void Undo()
+	{
+		On.DevInterface.DevUI.ctor -= DevUI_ctor;
+		On.DevInterface.DevUI.SwitchPage -= DevUI_SwitchPage;
+	}
+
+	private static void DevUI_SwitchPage(On.DevInterface.DevUI.orig_SwitchPage orig, DevUI self, int newPage)
+	{
+		if (newPage == self.pages.IndexOf("Background"))
 		{
-			On.DevInterface.DevUI.ctor += DevUI_ctor;
-			On.DevInterface.DevUI.SwitchPage += DevUI_SwitchPage;
+			self.ClearSprites();
+			self.activePage = new BackgroundPage(self, "Background_Page", null, "Background Settings");
 		}
 
+		else { orig(self, newPage); }
+	}
 
-		public static void Undo()
+	private static void DevUI_ctor(On.DevInterface.DevUI.orig_ctor orig, DevUI self, RainWorldGame game)
+	{
+		//this might need to be an il hook
+		self.game = game;
+		self.placedObjectsContainer = new FContainer();
+		if (game != null)
 		{
-			On.DevInterface.DevUI.ctor -= DevUI_ctor;
-			On.DevInterface.DevUI.SwitchPage -= DevUI_SwitchPage;
+			Futile.stage.AddChild(self.placedObjectsContainer);
 		}
-
-		private static void DevUI_SwitchPage(On.DevInterface.DevUI.orig_SwitchPage orig, DevUI self, int newPage)
+		self.pages = new string[]
 		{
-			if (newPage == self.pages.IndexOf("Background"))
-			{
-				self.ClearSprites();
-				self.activePage = new BackgroundPage(self, "Background_Page", null, "Background Settings");
-			}
-
-			else { orig(self, newPage); }
-		}
-
-		private static void DevUI_ctor(On.DevInterface.DevUI.orig_ctor orig, DevUI self, RainWorldGame game)
-		{
-			//this might need to be an il hook
-			self.game = game;
-			self.placedObjectsContainer = new FContainer();
-			if (game != null)
-			{
-				Futile.stage.AddChild(self.placedObjectsContainer);
-			}
-			self.pages = new string[]
-			{
 				"Room Settings",
 				"Objects",
 				"Sound",
@@ -55,81 +55,81 @@ namespace RegionKit.Modules.DevUIMisc
 				"Triggers",
 				"Dialog",
 				"Background"
-			};
+		};
 
-			self.SwitchPage(game.setupValues.defaultSettingsScreen);
+		self.SwitchPage(game.setupValues.defaultSettingsScreen);
 
+	}
+
+	public class BackgroundPage : Page
+	{
+		// Token: 0x060028F3 RID: 10483 RVA: 0x0031DEDC File Offset: 0x0031C0DC
+		public BackgroundPage(DevUI owner, string IDstring, DevUINode parentNode, string name) : base(owner, IDstring, parentNode, name)
+		{
+			subNodes.Add(new BackgroundTemplateCycler(owner, "Game_Over_Type_Cycler", this, new Vector2(170f, 660f), 120f));
 		}
 
-		public class BackgroundPage : Page
-		{
-			// Token: 0x060028F3 RID: 10483 RVA: 0x0031DEDC File Offset: 0x0031C0DC
-			public BackgroundPage(DevUI owner, string IDstring, DevUINode parentNode, string name) : base(owner, IDstring, parentNode, name)
-			{
-				subNodes.Add(new BackgroundTemplateCycler(owner, "Game_Over_Type_Cycler", this, new Vector2(170f, 660f), 120f));
-			}
+	}
 
+	public class BackgroundTemplateCycler : Button
+	{
+		// Token: 0x060028CD RID: 10445 RVA: 0x0031B114 File Offset: 0x00319314
+		public BackgroundTemplateCycler(DevUI owner, string IDstring, DevUINode parentNode, Vector2 pos, float width) : base(owner, IDstring, parentNode, pos, width, "")
+		{
+			subNodes.Add(new DevUILabel(owner, "Title", this, new Vector2(-50f, 0f), 40f, "G.O: "));
 		}
 
-		public class BackgroundTemplateCycler : Button
+		// Token: 0x060028CE RID: 10446 RVA: 0x0031B164 File Offset: 0x00319364
+
+		public BackgroundTemplateType templateType;
+		public override void Refresh()
 		{
-			// Token: 0x060028CD RID: 10445 RVA: 0x0031B114 File Offset: 0x00319314
-			public BackgroundTemplateCycler(DevUI owner, string IDstring, DevUINode parentNode, Vector2 pos, float width) : base(owner, IDstring, parentNode, pos, width, "")
+			base.Refresh();
+			string text;
+			if (RoomSettings.dType != null)
 			{
-				subNodes.Add(new DevUILabel(owner, "Title", this, new Vector2(-50f, 0f), 40f, "G.O: "));
+				text = "";
 			}
-
-			// Token: 0x060028CE RID: 10446 RVA: 0x0031B164 File Offset: 0x00319364
-
-			public BackgroundTemplateType templateType;
-			public override void Refresh()
+			else if (!RoomSettings.parent.isAncestor && RoomSettings.parent.dType != null)
 			{
-				base.Refresh();
-				string text;
-				if (RoomSettings.dType != null)
-				{
-					text = "";
-				}
-				else if (!RoomSettings.parent.isAncestor && RoomSettings.parent.dType != null)
-				{
-					text = "<T>";
-				}
-				else
-				{
-					text = "<A>";
-				}
-				string str = text;
-				string str2 = " ";
-				Text = str + str2 + (templateType != null ? templateType.ToString() : null);
+				text = "<T>";
 			}
-
-			// Token: 0x060028CF RID: 10447 RVA: 0x0031B1F8 File Offset: 0x003193F8
-			public override void Clicked()
+			else
 			{
-				if (templateType == null)
-				{
-					templateType = new BackgroundTemplateType(ExtEnum<BackgroundTemplateType>.values.GetEntry(0), false);
-				}
-
-				else
-				{
-					templateType = new BackgroundTemplateType(ExtEnum<BackgroundTemplateType>.values.GetEntry(templateType.Index + 1), false);
-				}
-				Refresh();
+				text = "<A>";
 			}
+			string str = text;
+			string str2 = " ";
+			Text = str + str2 + (templateType != null ? templateType.ToString() : null);
 		}
 
-		public class BackgroundTemplateType : ExtEnum<RoomRain.DangerType>
+		// Token: 0x060028CF RID: 10447 RVA: 0x0031B1F8 File Offset: 0x003193F8
+		public override void Clicked()
 		{
-			public BackgroundTemplateType(string value, bool register = false) : base(value, register)
+			if (templateType == null)
 			{
+				templateType = new BackgroundTemplateType(ExtEnum<BackgroundTemplateType>.values.GetEntry(0), false);
 			}
 
-			public static readonly BackgroundTemplateType AboveCloudsView = new BackgroundTemplateType("AboveCloudsView", true);
-
-			public static readonly BackgroundTemplateType RoofTopView = new BackgroundTemplateType("RoofTopView", true);
-
-			public static readonly BackgroundTemplateType VoidSeaScene = new BackgroundTemplateType("VoidSeaScene", true);
+			else
+			{
+				templateType = new BackgroundTemplateType(ExtEnum<BackgroundTemplateType>.values.GetEntry(templateType.Index + 1), false);
+			}
+			Refresh();
 		}
 	}
+
+	public class BackgroundTemplateType : ExtEnum<RoomRain.DangerType>
+	{
+		public BackgroundTemplateType(string value, bool register = false) : base(value, register)
+		{
+		}
+
+		public static readonly BackgroundTemplateType AboveCloudsView = new BackgroundTemplateType("AboveCloudsView", true);
+
+		public static readonly BackgroundTemplateType RoofTopView = new BackgroundTemplateType("RoofTopView", true);
+
+		public static readonly BackgroundTemplateType VoidSeaScene = new BackgroundTemplateType("VoidSeaScene", true);
+	}
 }
+
--- src/Modules/DevUIMisc/SettingsSaveOptions.cs
+++ src/Modules/DevUIMisc/SettingsSaveOptions.cs
@@ -8,134 +8,130 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using System.IO;
 
-namespace RegionKit.Modules.DevUIMisc
+namespace RegionKit.Modules.DevUIMisc;
+
+internal class SettingsSaveOptions
 {
-	internal class SettingsSaveOptions
+	public static void SaveSignal(Page self, DevUISignalType type, DevUINode sender, string message)
 	{
+		if (settingsSaveOptionsMenu is null)
+		{ return; }
 
-
-		public static void SaveSignal(Page self, DevUISignalType type, DevUINode sender, string message)
+		if (sender.IDstring == "Change_Path")
 		{
-			if (settingsSaveOptionsMenu is null)
-			{ return; }
-
-			if (sender.IDstring == "Change_Path")
+			//remove panel with no changes when button clicked
+			if (settingsSaveOptionsMenu.modSelectPanel != null)
 			{
-				//remove panel with no changes when button clicked
-				if (settingsSaveOptionsMenu.modSelectPanel != null)
-				{
-					self.subNodes.Remove(settingsSaveOptionsMenu.modSelectPanel);
-					settingsSaveOptionsMenu.modSelectPanel.ClearSprites();
-					settingsSaveOptionsMenu.modSelectPanel = null;
-					return;
-				}
-
-				//create new panel when button clicked
-				settingsSaveOptionsMenu.modSelectPanel = new ItemSelectPanel(self.owner, self, new Vector2(420f, 250f), settingsSaveOptionsMenu.modNames.Keys.ToArray(), "ModPanel", "Select Mod");
-				self.subNodes.Add(settingsSaveOptionsMenu.modSelectPanel);
+				self.subNodes.Remove(settingsSaveOptionsMenu.modSelectPanel);
+				settingsSaveOptionsMenu.modSelectPanel.ClearSprites();
+				settingsSaveOptionsMenu.modSelectPanel = null;
 				return;
 			}
 
-			else if (settingsSaveOptionsMenu.modSelectPanel != null && sender.IDstring.StartsWith(settingsSaveOptionsMenu.modSelectPanel.idstring + "Button99289_"))
+			//create new panel when button clicked
+			settingsSaveOptionsMenu.modSelectPanel = new ItemSelectPanel(self.owner, self, new Vector2(420f, 250f), settingsSaveOptionsMenu.modNames.Keys.ToArray(), "ModPanel", "Select Mod");
+			self.subNodes.Add(settingsSaveOptionsMenu.modSelectPanel);
+			return;
+		}
+
+		else if (settingsSaveOptionsMenu.modSelectPanel != null && sender.IDstring.StartsWith(settingsSaveOptionsMenu.modSelectPanel.idstring + "Button99289_"))
+		{
+			string subbuttonid = sender.IDstring.Remove(0, (settingsSaveOptionsMenu.modSelectPanel.idstring + "Button99289_").Length);
+
+			if (settingsSaveOptionsMenu.ChangePath != null)
+			{ settingsSaveOptionsMenu.ChangePath.Text = subbuttonid; }
+
+
+			if (DevUIUtils.URoomSettings.PathToSpecificSettings(settingsSaveOptionsMenu.modNames[subbuttonid], self.RoomSettings.name, out string filePath))
 			{
-				string subbuttonid = sender.IDstring.Remove(0, (settingsSaveOptionsMenu.modSelectPanel.idstring + "Button99289_").Length);
-
-				if (settingsSaveOptionsMenu.ChangePath != null)
-				{ settingsSaveOptionsMenu.ChangePath.Text = subbuttonid; }
-
-
-				if (DevUIUtils.URoomSettings.PathToSpecificSettings(settingsSaveOptionsMenu.modNames[subbuttonid], self.RoomSettings.name, out string filePath))
-				{
-					self.RoomSettings.filePath = filePath;
-					Debug.Log($"new filepath is [{filePath}]");
-					Debug.Log(subbuttonid);
-					Debug.Log(settingsSaveOptionsMenu.modNames[subbuttonid]);
-					settingsSaveOptionsMenu.RefreshPathLabel();
-				}
-
-				if (settingsSaveOptionsMenu.modSelectPanel != null)
-				{
-					self.subNodes.Remove(settingsSaveOptionsMenu.modSelectPanel);
-					settingsSaveOptionsMenu.modSelectPanel.ClearSprites();
-					settingsSaveOptionsMenu.modSelectPanel = null;
-				}
+				self.RoomSettings.filePath = filePath;
+				Debug.Log($"new filepath is [{filePath}]");
+				Debug.Log(subbuttonid);
+				Debug.Log(settingsSaveOptionsMenu.modNames[subbuttonid]);
+				settingsSaveOptionsMenu.RefreshPathLabel();
 			}
 
-			else if (sender.IDstring == "Create_Modify" && settingsSaveOptionsMenu.ChangePath != null)
+			if (settingsSaveOptionsMenu.modSelectPanel != null)
 			{
-				new ModifySettingsGenerator(self.owner, settingsSaveOptionsMenu.ChangePath.Text);
+				self.subNodes.Remove(settingsSaveOptionsMenu.modSelectPanel);
+				settingsSaveOptionsMenu.modSelectPanel.ClearSprites();
+				settingsSaveOptionsMenu.modSelectPanel = null;
 			}
 		}
 
-
-		public static SettingsSaveOptionsMenu? settingsSaveOptionsMenu;
-
-
-		public class SettingsSaveOptionsMenu : DevUINode
+		else if (sender.IDstring == "Create_Modify" && settingsSaveOptionsMenu.ChangePath != null)
 		{
-			public SettingsSaveOptionsMenu(DevUI owner, string IDstring, DevUINode parentNode) : base(owner, IDstring, parentNode)
-			{
+			new ModifySettingsGenerator(self.owner, settingsSaveOptionsMenu.ChangePath.Text);
+		}
+	}
 
-				modNames = new Dictionary<string, string>() {
+
+	public static SettingsSaveOptionsMenu? settingsSaveOptionsMenu;
+
+
+	public class SettingsSaveOptionsMenu : DevUINode
+	{
+		public SettingsSaveOptionsMenu(DevUI owner, string IDstring, DevUINode parentNode) : base(owner, IDstring, parentNode)
+		{
+
+			modNames = new Dictionary<string, string>() {
 				{"Default", DevUIUtils.URoomSettings.DefaultSettingsLocation(owner, RoomSettings, false)},
 				{"vanilla", RootFolderDirectory()},
 				{"mergedmods",Path.Combine(RootFolderDirectory(), "mergedmods") }
 			};
 
-				foreach (ModManager.Mod mod in ModManager.ActiveMods)
-				{
-					modNames.Add(mod.name, mod.path);
-				}
-
-				RefreshPathLabel();
-
-				//SavedPath = new DevUILabel(owner, "Saved_Path", null, new Vector2(900f, 700f), 100f, "Default");
-
-				//self.subNodes.Add(SavedPath);
-
-				ChangePath = new Button(owner, "Change_Path", this, new Vector2(790f, 700f), 100f, "Default");
-
-				subNodes.Add(ChangePath);
-
-
-				CreateModify = new Button(owner, "Create_Modify", this, new Vector2(900f, 700f), 100f, "Create Modify");
-
-				subNodes.Add(CreateModify);
-			}
-
-			public void RefreshPathLabel()
+			foreach (ModManager.Mod mod in ModManager.ActiveMods)
 			{
-				if (SettingsPathLabel != null && subNodes.Contains(SettingsPathLabel))
-				{
-					subNodes.Remove(SettingsPathLabel);
-					SettingsPathLabel.ClearSprites();
-					SettingsPathLabel = null;
-					Debug.Log("removed settingspath");
-				}
-
-				string settingsPath = "Settings Path: " + RoomSettings.filePath.ToLower();
-
-				DevUIUtils.UPath.TryCropToSubstringRight(settingsPath, "workshop", out settingsPath);
-
-				DevUIUtils.UPath.TryCropToSubstringRight(settingsPath, "streamingassets", out settingsPath);
-
-				Debug.Log("adding settingspath\n"+settingsPath);
-				SettingsPathLabel = new DevUILabel(owner, "Settings_Path", this, new Vector2(1330f - 6f * settingsPath.Length, 20f), 20f + 6f * settingsPath.Length, settingsPath);
-
-				subNodes.Add(SettingsPathLabel);
+				modNames.Add(mod.name, mod.path);
 			}
 
-			public ItemSelectPanel? modSelectPanel;
+			RefreshPathLabel();
 
-			public DevUILabel? SettingsPathLabel = null;
+			//SavedPath = new DevUILabel(owner, "Saved_Path", null, new Vector2(900f, 700f), 100f, "Default");
 
-			public Button ChangePath;
+			//self.subNodes.Add(SavedPath);
 
-			public Button CreateModify;
+			ChangePath = new Button(owner, "Change_Path", this, new Vector2(790f, 700f), 100f, "Default");
 
-			public Dictionary<string, string> modNames;
+			subNodes.Add(ChangePath);
 
+
+			CreateModify = new Button(owner, "Create_Modify", this, new Vector2(900f, 700f), 100f, "Create Modify");
+
+			subNodes.Add(CreateModify);
 		}
+
+		public void RefreshPathLabel()
+		{
+			if (SettingsPathLabel != null && subNodes.Contains(SettingsPathLabel))
+			{
+				subNodes.Remove(SettingsPathLabel);
+				SettingsPathLabel.ClearSprites();
+				SettingsPathLabel = null;
+				Debug.Log("removed settingspath");
+			}
+
+			string settingsPath = "Settings Path: " + RoomSettings.filePath.ToLower();
+
+			DevUIUtils.UPath.TryCropToSubstringRight(settingsPath, "workshop", out settingsPath);
+
+			DevUIUtils.UPath.TryCropToSubstringRight(settingsPath, "streamingassets", out settingsPath);
+
+			Debug.Log("adding settingspath\n" + settingsPath);
+			SettingsPathLabel = new DevUILabel(owner, "Settings_Path", this, new Vector2(1330f - 6f * settingsPath.Length, 20f), 20f + 6f * settingsPath.Length, settingsPath);
+
+			subNodes.Add(SettingsPathLabel);
+		}
+
+		public ItemSelectPanel? modSelectPanel;
+
+		public DevUILabel? SettingsPathLabel = null;
+
+		public Button ChangePath;
+
+		public Button CreateModify;
+
+		public Dictionary<string, string> modNames;
 
 	}
 }

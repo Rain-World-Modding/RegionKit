55da7b01b62f20686991c8c60bb4c5b6e5c8949c
SortaUnknown <roi.anubis@gmail.com>
2023-03-31 22:52:32 +0200

    Troubleshooting stuff

--- src/Modules/Misc/RainSong.cs
+++ src/Modules/Misc/RainSong.cs
@@ -22,6 +22,8 @@ internal static class RainSong
 
     static bool filesChecked = false;
 
+    static string  debugName;
+
     static readonly Dictionary<string, string> rainSongDict = new();
 
     static void GameCtorPatch(On.RainWorldGame.orig_ctor orig, RainWorldGame self, ProcessManager manager)
@@ -30,14 +32,16 @@ internal static class RainSong
 
         if (!filesChecked)
         {
-            string[] songArray = AssetManager.ListDirectory("music" + Path.DirectorySeparatorChar + "songs").Where(s => s.Contains("RAINSONG")).ToArray();
+            Debug.Log("RainSong:  Checking song files...");
+            string[] songArray = AssetManager.ListDirectory("music" + Path.DirectorySeparatorChar + "songs");
+            Debug.Log("RainSong:  Song Array: " + songArray.Length);
             if (songArray.Length != 0)
             {
                 foreach (string song in songArray)
                 {
                     string songName = Path.GetFileNameWithoutExtension(song);
                     string key = songName.Remove(2);
-                    if (!rainSongDict.Keys.Contains(key))
+                    if (songName.ToLower().Contains("rainsong") && !rainSongDict.ContainsKey(key))
                     {
                         rainSongDict.Add(key, songName);
                         Debug.Log("RainSong:  Registered Rain Song named " + song);
@@ -47,7 +51,9 @@ internal static class RainSong
 
             filesChecked = true;
         }
-        if (manager.musicPlayer != null && manager.musicPlayer.song != null && manager.musicPlayer.song.name.Contains("RAINSONG")) manager.musicPlayer.song.FadeOut(100f);
+
+        debugName = rainSongDict.Values.ToArray()[0];
+        if (manager.musicPlayer != null && manager.musicPlayer.song != null && manager.musicPlayer.song.name.ToLower().Contains("rainsong")) manager.musicPlayer.song.FadeOut(100f);
     }
 
     static void RainUpdatePatch(On.RainCycle.orig_Update orig, RainCycle self)
@@ -55,10 +61,13 @@ internal static class RainSong
         orig.Invoke(self);
         RainWorldGame game = self.world.game;
         MusicPlayer player = game.manager.musicPlayer;
-        if (self.RainApproaching < 0.5f && player != null && game.cameras[0].room.roomSettings.DangerType != RoomRain.DangerType.None && player.song == null && !game.cameras[0].room.abstractRoom.name.Contains("GATE") && !game.Players[0].realizedCreature.dead && rainSongDict.TryGetValue(self.world.region.name, out string songName))
+        string songName = null;
+        bool check = self.RainApproaching < 0.5f && player != null && game.cameras[0].room.roomSettings.DangerType != RoomRain.DangerType.None && player.song == null && !game.cameras[0].room.abstractRoom.name.Contains("GATE") && !game.Players[0].realizedCreature.dead && rainSongDict.TryGetValue(self.world.region.name, out songName);
+        //Debug.Log("RainSong:  " + check);
+        if (player != null && player.song == null)
         {
             Debug.Log("RainSong:  Playing end of cycle song");
-			Song song = new(game.manager.musicPlayer, songName, MusicPlayer.MusicContext.StoryMode)
+			Song song = new(game.manager.musicPlayer, debugName, MusicPlayer.MusicContext.StoryMode)
 			{
                 playWhenReady = true,
 				Loop = true,
@@ -73,7 +82,8 @@ internal static class RainSong
 
     static void RainStopPatch(On.Music.MusicPlayer.orig_RainRequestStopSong orig, MusicPlayer self)
     {
-        if (self.song != null && self.song.name.Contains("RAINSONG")) return;
+        if (self.song != null && self.song.name.ToLower().Contains("rainsong")) return;
+        Debug.Log("RainSong:  are you sure this should happen");
         orig.Invoke(self);
     }
 }

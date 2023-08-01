11a926fa307fe6e76804b0bc4f70b5754208ba57
thalber <filefillman@gmail.com>
2023-08-01 05:09:18 +0400

    better modnotinit error message

--- src/ModNotInitializedException.cs
+++ src/ModNotInitializedException.cs
@@ -2,7 +2,7 @@ namespace RegionKit;
 
 public class ModNotInitializedException : Exception
 {
-	public ModNotInitializedException(string? message = null) : base(message ?? "RegionKit has not been initialized; add a BepInDependency")
+	public ModNotInitializedException(string? message = null) : base(message ?? "RegionKit has not been initialized; add a BepInDependency attributeto your plugin to make sure RegionKit loads first.")
 	{
 
 	}

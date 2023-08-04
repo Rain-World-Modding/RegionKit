c67ae0537465bd4bfdf31e7c797861c0e63aaf80
SortaUnknown <roi.anubis@gmail.com>
2023-08-04 22:33:22 +0200

    Add Rain Song

--- src/Modules/Misc/_Module.cs
+++ src/Modules/Misc/_Module.cs
@@ -19,6 +19,7 @@ internal static class _Module
 		SuperstructureFusesHook.Apply();
 		MoreFadePalettes.Apply();
 		SlugcatRoomTemplates.Apply();
+		RainSong.Enable();
 	}
 	public static void Disable()
 	{
@@ -29,5 +30,6 @@ internal static class _Module
 		SuperstructureFusesHook.Undo();
 		MoreFadePalettes.Undo();
 		SlugcatRoomTemplates.Undo();
+		RainSong.Disable();
 	}
 }

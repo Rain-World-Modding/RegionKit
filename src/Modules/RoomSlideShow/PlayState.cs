1cabb1989e3a1803303d4cc57a900d7bdc585f0b
thalber <filefillman@gmail.com>
2023-08-24 06:29:08 +0400

    add a method to build next/prev kf cache

--- src/Modules/RoomSlideShow/PlayState.cs
+++ src/Modules/RoomSlideShow/PlayState.cs
@@ -1,12 +1,16 @@
+using System.Linq;
+
 namespace RegionKit.Modules.Slideshow;
 
 internal sealed class PlayState
 {
+	//todo: test ALL OF THIS SHIT
 	private const int MAX_INSTANT_INSTRUCTIONS = 1024;
-	//private readonly Dictionary<Channel, KeyFrame> startKeyFrames;
-	//private readonly Dictionary<Channel, KeyFrame> endKeyFrames;
+	private readonly Dictionary<Channel, KeyFrame> startKeyFrames;
+	private readonly Dictionary<Channel, KeyFrame> endKeyFrames;
+
 	private readonly Dictionary<Channel, KeyFrame> lastKeyFrames = new();
-	private readonly Dictionary<Channel, KeyFrame> upcomingKeyFrames = new();
+	private readonly Dictionary<Channel, KeyFrame> nextKeyFrames = new();
 	private readonly Dictionary<Channel, SetInterpolation> interpolationSettings = new();
 	public PlaybackStep CurrentStep => this.owner.playbackSteps[this.CurrentIndex];
 	public Frame CurrentFrame => (Frame)this.CurrentStep;
@@ -19,17 +23,33 @@ internal sealed class PlayState
 
 	public PlayState(Playback owner)
 	{
-		// this.startKeyFrames = new() {
-		// 	{ Channel.X, new(0, Channel.X, 0f)},
-		// 	{ Channel.Y, new(0, Channel.Y, 0f)},
-		// 	{ Channel.R, new(0, Channel.R, 1f)},
-		// 	{ Channel.G, new(0, Channel.G, 1f)},
-		// 	{ Channel.B, new(0, Channel.B, 1f)},
-		// 	{ Channel.A, new(0, Channel.A, 1f)},
-		// };
 		this.owner = owner;
+		this.startKeyFrames = CreateDefaultKeyframes(0);
+		this.endKeyFrames = CreateDefaultKeyframes(owner.playbackSteps.Length - 1);
+		this.interpolationSettings = CreateDefaultInterpolations();
 	}
-
+	private static Dictionary<Channel, SetInterpolation> CreateDefaultInterpolations()
+	{
+		return new() {
+			{ Channel.X, new SetInterpolation(InterpolationKind.No, new[] {Channel.X})},
+			{ Channel.Y, new SetInterpolation(InterpolationKind.No, new[] {Channel.Y})},
+			{ Channel.R, new SetInterpolation(InterpolationKind.No, new[] {Channel.R})},
+			{ Channel.G, new SetInterpolation(InterpolationKind.No, new[] {Channel.G})},
+			{ Channel.B, new SetInterpolation(InterpolationKind.No, new[] {Channel.B})},
+			{ Channel.A, new SetInterpolation(InterpolationKind.No, new[] {Channel.A})},
+		};
+	}
+	private static Dictionary<Channel, KeyFrame> CreateDefaultKeyframes(int at)
+	{
+		return new() {
+				{ Channel.X, new(at, Channel.X, 0f)},
+				{ Channel.Y, new(at, Channel.Y, 0f)},
+				{ Channel.R, new(at, Channel.R, 1f)},
+				{ Channel.G, new(at, Channel.G, 1f)},
+				{ Channel.B, new(at, Channel.B, 1f)},
+				{ Channel.A, new(at, Channel.A, 1f)},
+			};
+	}
 	public void Update()
 	{
 		bool overstayed = true;
@@ -42,10 +62,10 @@ internal sealed class PlayState
 				DefaultFrameDelay = setDelay.newDelay;
 				break;
 
-			case SetInterpolation setSmooth:
-				foreach (var channel in setSmooth.channels)
+			case SetInterpolation setInterpol:
+				foreach (var channel in setInterpol.channels)
 				{
-					interpolationSettings[channel] = setSmooth;
+					interpolationSettings[channel] = setInterpol;
 				}
 				break;
 			case SetShader setShader:
@@ -64,6 +84,7 @@ internal sealed class PlayState
 				break;
 			}
 			CurrentIndex++;
+			UpdateKeyFrames();
 		}
 		if (overstayed)
 		{
@@ -71,9 +92,41 @@ internal sealed class PlayState
 		}
 		TicksSinceStart++;
 	}
+	public void UpdateKeyFrames()
+	{
+		//move keyframes that have been hit to last
+		List<KeyFrame> fromNextToLast = new();
+		foreach ((Channel channel, KeyFrame keyframe) in nextKeyFrames)
+		{
+			if (keyframe.atFrame == CurrentIndex)
+			{
+				fromNextToLast.Add(keyframe);
+			}
+		}
+		foreach (KeyFrame keyframe in fromNextToLast)
+		{
+			nextKeyFrames.Remove(keyframe.channel);
+			lastKeyFrames[keyframe.channel] = keyframe;
+		}
+		//search for upcoming keyframes
+		var allChannelsToCheck = ((Channel[])Enum.GetValues(typeof(Channel))).Where(item => !nextKeyFrames.ContainsKey(item)).ToList();
+		for (int i = CurrentIndex; i < owner.playbackSteps.Length; i++)
+		{
+			PlaybackStep step = owner.playbackSteps[i];
+			if (step is not Frame frame) continue;
+			foreach (KeyFrame keyFrame in frame.keyFramesHere)
+			{
+				if (allChannelsToCheck.Contains(keyFrame.channel))
+				{
+					nextKeyFrames[keyFrame.channel] = keyFrame;
+				}
+			}
+		}
+		//i hate this lmao
+	}
 	public SetInterpolation GetInterpolationSetting(Channel channel) => interpolationSettings[channel];
-	public KeyFrame GetLastKeyFrame(Channel channel) => lastKeyFrames[channel];
-	public KeyFrame GetUpcomingKeyFrame(Channel channel) => upcomingKeyFrames[channel];
+	public KeyFrame GetLastKeyFrame(Channel channel) => lastKeyFrames.TryGetValue(channel, out KeyFrame frame) ? frame : startKeyFrames[channel];
+	public KeyFrame GetUpcomingKeyFrame(Channel channel) => nextKeyFrames.TryGetValue(channel, out KeyFrame frame) ? frame : endKeyFrames[channel];
 	public int CurrentTransitionFrames(Channel channel) => GetLastKeyFrame(channel).atFrame - GetUpcomingKeyFrame(channel).atFrame;
 	public int CurrentTransitionTicks(Channel channel) => CountTickLengths(GetLastKeyFrame(channel).atFrame, GetUpcomingKeyFrame(channel).atFrame);
 	private int CountTickLengths(int from, int to)

1fedc8354353b07fab53be239feccc936b13cbe8
thalber <filefillman@gmail.com>
2023-09-02 20:19:43 +0400

    split some slideshow code off into subfolders

--- src/Modules/RoomSlideShow/SlideShowMeshData.cs
+++ src/Modules/RoomSlideShow/SlideShowMeshData.cs
@@ -10,16 +10,3 @@ public sealed class SlideShowMeshData : ManagedData
 	{
 	}
 }
-
-public sealed class SlideShowRectData : ManagedData
-{
-	[StringField("00id", "test", "Id")]
-	public string id = "test";
-	[Vector2Field("01p2", 100f, 100f, Vector2Field.VectorReprType.rect)]
-	public Vector2 p2;
-	public SlideShowRectData(PlacedObject owner) : base(owner, null)
-	{
-
-	}
-
-}
---
+++ src/Modules/RoomSlideShow/SlideShowRectData.cs
@@ -0,0 +1,14 @@
+namespace RegionKit.Modules.Slideshow;
+
+public sealed class SlideShowRectData : ManagedData
+{
+	[StringField("00id", "test", "Id")]
+	public string id = "test";
+	[Vector2Field("01p2", 100f, 100f, Vector2Field.VectorReprType.rect)]
+	public Vector2 p2;
+	public SlideShowRectData(PlacedObject owner) : base(owner, null)
+	{
+
+	}
+
+}

7737b98ceab41a19ebd0d7774a0abe76f3de3aa4
Isbjorn52 <isak.hellfalk@gmail.com>
2023-10-18 16:27:04 +0200

    Added tabs so placed objects wont go offscreen

--- src/Modules/IndividualPlacedObjectViewer/IndividualPlacedObjectViewer.cs
+++ src/Modules/IndividualPlacedObjectViewer/IndividualPlacedObjectViewer.cs
@@ -11,13 +11,11 @@ internal static class IndividualPlacedObjectViewer
 
 	private class ObjectsPageData
 	{
-		public bool isInIndividualMode;
-		public int currentPlacedObject = -1;
+		public bool isInIndividualMode = false;
+		public List<int> visiblePlacedObjects = new List<int>();
+		public int page = 0;
 
-		public ObjectsPageData()
-		{
-			isInIndividualMode = false;
-		}
+		public ObjectsPageData() { }
 	}
 
 	private static ObjectsPageData GetData(this ObjectsPage objectsPage)
@@ -33,12 +31,10 @@ internal static class IndividualPlacedObjectViewer
 
 	public static void Enable()
 	{
-		// Hooks, alot?
 		On.DevInterface.ObjectsPage.ctor += ObjectsPage_ctor;
 		On.DevInterface.ObjectsPage.Signal += ObjectsPage_Signal;
 
 		IL.DevInterface.ObjectsPage.Refresh += IL_ObjectsPage_Refresh;
-
 	}
 
 
@@ -46,6 +42,7 @@ internal static class IndividualPlacedObjectViewer
 	{
 		On.DevInterface.ObjectsPage.ctor -= ObjectsPage_ctor;
 		On.DevInterface.ObjectsPage.Signal -= ObjectsPage_Signal;
+
 		IL.DevInterface.ObjectsPage.Refresh -= IL_ObjectsPage_Refresh;
 	}
 
@@ -54,7 +51,6 @@ internal static class IndividualPlacedObjectViewer
 		orig(self, owner, IDstring, parentNode, name);
 
 		self.subNodes.Add(new Button(owner, "Switch_Mode", self, new Vector2(180f, 710f), 95f, "Switch mode"));
-
 	}
 
 	private static void ObjectsPage_Signal(On.DevInterface.ObjectsPage.orig_Signal orig, ObjectsPage self, DevUISignalType type, DevUINode sender, string message)
@@ -64,23 +60,45 @@ internal static class IndividualPlacedObjectViewer
 		if (sender.IDstring == "Switch_Mode")
 		{
 			self.GetData().isInIndividualMode = !self.GetData().isInIndividualMode;
-			Debug.Log("Switch_Mode was pressed!");
-			Debug.Log("Mode is now: " + self.GetData().isInIndividualMode.ToString());
+			//Debug.Log("Switch_Mode was pressed!");
+			//Debug.Log("Mode is now: " + self.GetData().isInIndividualMode.ToString());
 
-			// Switch mode
 			self.Refresh();
 		}
 		if (sender.IDstring.Contains("Placed_Object_Button_"))
 		{
-			Debug.Log(sender.IDstring[21..]);
-			int.TryParse(sender.IDstring[21..], out int bruh);
-			self.GetData().currentPlacedObject = bruh;
+			//Debug.Log(sender.IDstring[21..]);
+			int.TryParse(sender.IDstring[21..], out int placedObjectIndex);
+			self.GetData().visiblePlacedObjects.Clear();
+			self.GetData().visiblePlacedObjects.Add(placedObjectIndex);
+			self.Refresh();
+		}
+
+		if (sender.IDstring == "Prev_Page_Button") 
+		{ 
+			self.GetData().page--;
+
+			int numberOfPages = self.RoomSettings.placedObjects.Count / 24 + 1;
+			if (self.GetData().page < 0) self.GetData().page += numberOfPages;
+			self.GetData().page %= numberOfPages;
+
+			self.Refresh();
+		}
+		if (sender.IDstring == "Next_Page_Button") 
+		{
+			self.GetData().page++;
+
+			int numberOfPages = self.RoomSettings.placedObjects.Count / 24 + 1;
+			if (self.GetData().page < 0) self.GetData().page += numberOfPages;
+			self.GetData().page %= numberOfPages;
+
 			self.Refresh();
 		}
 
 		if (type == DevUISignalType.Create)
 		{
-			self.GetData().currentPlacedObject = self.RoomSettings.placedObjects.Count - 1;
+			self.GetData().visiblePlacedObjects.Clear();
+			self.GetData().visiblePlacedObjects.Add(self.RoomSettings.placedObjects.Count - 1);
 			self.Refresh();
 		}
 	}
@@ -91,38 +109,39 @@ internal static class IndividualPlacedObjectViewer
 		//orig(self); 
 
 		ILCursor cursor = new ILCursor(il);
-
 		cursor.Index = 2;
-		Debug.Log(cursor.Instrs.Count);
-		for (int i = 0; i < cursor.Instrs.Count; i++)
-		{
-			Debug.Log(cursor.Instrs[i].OpCode.ToString());
-		}
-		Debug.Log("-------------------");
-
 		cursor.RemoveRange(26);
-		for (int i = 0; i < cursor.Instrs.Count; i++)
-		{
-			Debug.Log(cursor.Instrs[i].OpCode.ToString());
-		}
-
-
 		cursor.Emit(OpCodes.Ldarg_0);
 
 		cursor.EmitDelegate<Action<ObjectsPage>>((self) => 
 		{
 			for (int i = 0; i < self.RoomSettings.placedObjects.Count; i++)
 			{
-				if (self.GetData().currentPlacedObject == i || !self.GetData().isInIndividualMode) self.CreateObjRep(self.RoomSettings.placedObjects[i].type, self.RoomSettings.placedObjects[i]);
+				if (self.GetData().visiblePlacedObjects.Contains(i) || !self.GetData().isInIndividualMode) 
+				{ 
+					self.CreateObjRep(self.RoomSettings.placedObjects[i].type, self.RoomSettings.placedObjects[i]); 
+				}
 			}
 
 			if (self.GetData().isInIndividualMode)
 			{
-				PlacedObjectsPanel panel = new PlacedObjectsPanel(self.owner, "All_Objects_Panel", self, new Vector2(1050f, 20f), new Vector2(300f, 650f), "Placed Objects");
+				PlacedObjectsPanel panel = new PlacedObjectsPanel(self.owner, "All_Objects_Panel", self, new Vector2(1050f, 40f), new Vector2(300f, 600f), "Placed Objects");
+
+				panel.subNodes.Add(new Panel.HorizontalDivider(self.owner, "Divider", panel, panel.size.y - 80f));
+
+				int numberOfPages = self.RoomSettings.placedObjects.Count / 24 + 1;
+
+				panel.subNodes.Add(new Button(self.owner, "Prev_Page_Button", panel, new Vector2(5f, panel.size.y - 100), 100f, "Prev Page"));
+				panel.subNodes.Add(new Button(self.owner, "Next_Page_Button", panel, new Vector2(195f, panel.size.y - 100), 100f, "Next Page"));
 
 				for (int i = 0; i < self.RoomSettings.placedObjects.Count; i++)
 				{
-					panel.subNodes.Add(new Button(self.owner, $"Placed_Object_Button_{i}", panel, new Vector2(5f, panel.size.y - 20f * (i + 1)), 290, $"{i} {self.RoomSettings.placedObjects[i].type.ToString()}"));
+					if(i/24 == self.GetData().page)
+					{
+						panel.subNodes.Add(new Button(self.owner, $"Placed_Object_Button_{i}", panel, new Vector2(5f, panel.size.y - 140f - 20f * (i%24)), 290, $"{i} {self.RoomSettings.placedObjects[i].type.ToString()}"));
+
+					}
+
 				}
 
 				self.tempNodes.Add(panel);

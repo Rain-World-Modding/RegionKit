1f35b8de31f46d9d475a918f25a4ef4301111d86
Bro <bro479508@gmail.com>
2023-12-25 12:30:37 -0700

    ReliableIggyEntrance
    Object that specifies where Iggy will enter a room from

--- src/Modules/CustomProjections/_Module.cs
+++ src/Modules/CustomProjections/_Module.cs
@@ -4,15 +4,20 @@
 [RegionKitModule(nameof(Enable), nameof(Disable), nameof(Setup), moduleName: "CustomProjections")]
 public static class _Module
 {
+	public const string OVERSEER_POM_CATEGORY = RK_POM_CATEGORY + "-CustomProjections";
 	internal static void Setup()
 	{
+		RegisterManagedObject<ReliableIggyEntrance, ReliableEntranceData, ReliableEntranceRep>("ReliableIggyEntrance", OVERSEER_POM_CATEGORY);
 	}
 
 	internal static void Enable()
 	{
 		CustomProjections.Apply();
+		ReliableIggyEntrance.Apply();
 	}
 	internal static void Disable()
 	{
+		CustomProjections.Undo();
+		ReliableIggyEntrance.Undo();
 	}
 }

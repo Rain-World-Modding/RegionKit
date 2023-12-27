6bd92b642909b7d2597da584a0d852efb69990e3
Fyëryn <67385290+M4rbleL1ne@users.noreply.github.com>
2023-12-27 18:11:47 +0100

    Misc Fixes
    Spinning Fan rotation + little perf improvements (notably MosquitoInsects Mosquito creature check)

--- src/Modules/Objects/NoWallSlideZones.cs
+++ src/Modules/Objects/NoWallSlideZones.cs
@@ -2,6 +2,7 @@
 using static System.Text.RegularExpressions.Regex;
 using static UnityEngine.Mathf;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace RegionKit.Modules.Objects;
 
@@ -26,10 +27,11 @@ public static class NoWallSlideZones
 	private static void PlayerUpdate(On.Player.orig_Update orig, Player self, bool eu)
 	{
 		orig(self, eu);
-		if (self.bodyMode == Player.BodyModeIndex.WallClimb && self.InsideNWSRects() && !self.submerged && self.bodyChunks[0].ContactPoint.x != 0 && self.bodyChunks[0].ContactPoint.x == self.input[0].x)
+		BodyChunk[] bs = self.bodyChunks;
+		if (self.bodyMode == Player.BodyModeIndex.WallClimb && self.InsideNWSRects() && !self.submerged && bs[0].ContactPoint.x != 0 && bs[0].ContactPoint.x == self.input[0].x)
 		{
-			foreach (BodyChunk b in self.bodyChunks)
-				b.contactPoint.x = 0;
+			for (var i = 0; i < bs.Length; i++)
+				bs[i].contactPoint.x = 0;
 			self.bodyMode = Player.BodyModeIndex.Default;
 			self.animation = Player.AnimationIndex.None;
 		}
@@ -70,12 +72,21 @@ public class NoWallSlideZone : UpdatableAndDeletable
 	}
 }
 
-internal class FloatRectData : PlacedObject.Data
+internal sealed class FloatRectData : PlacedObject.Data
 {
 	public Vector2 handlePos;
 
-	public virtual FloatRect Rect => new(Min(owner.pos.x, owner.pos.x + handlePos.x), Min(owner.pos.y, owner.pos.y + handlePos.y), Max(owner.pos.x, owner.pos.x + handlePos.x), Max(owner.pos.y, owner.pos.y + handlePos.y));
+	public FloatRect Rect
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get
+		{
+			float posX = owner.pos.x, posY = owner.pos.y, hPosX = handlePos.x, hPosY = handlePos.y;
+			return new(Min(posX, posX + hPosX), Min(posY, posY + handlePos.y), Max(posX, posX + hPosX), Max(posY, posY + hPosY));
+		}
+	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public FloatRectData(PlacedObject owner) : base(owner) => handlePos = new(80f, 80f);
 
 	public override void FromString(string s)
@@ -86,12 +97,16 @@ internal class FloatRectData : PlacedObject.Data
 		unrecognizedAttributes = SaveUtils.PopulateUnrecognizedStringAttrs(sAr, 2);
 	}
 
-	public override string ToString() => SaveUtils.AppendUnrecognizedStringAttrs($"{handlePos.x}~{handlePos.y}", "~", unrecognizedAttributes);
+	public override string ToString() => SaveUtils.AppendUnrecognizedStringAttrs(SaveState.SetCustomData(this, $"{handlePos.x}~{handlePos.y}"), "~", unrecognizedAttributes);
 }
 
-internal class FloatRectRepresentation : PlacedObjectRepresentation
+internal sealed class FloatRectRepresentation : PlacedObjectRepresentation
 {
-	public virtual FloatRectData Data => (pObj.data as FloatRectData)!;
+	private FloatRectData Data
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => (pObj.data as FloatRectData)!;
+	}
 
 	public FloatRectRepresentation(DevUI owner, string IDstring, DevUINode parentNode, PlacedObject pObj, string name) : base(owner, IDstring, parentNode, pObj, name)
 	{
@@ -112,51 +127,59 @@ internal class FloatRectRepresentation : PlacedObjectRepresentation
 	public override void Refresh()
 	{
 		base.Refresh();
-		Vector2 camPos = owner.room.game.cameras[0].pos;
+		List<FSprite> fsprs = fSprites;
+		Vector2 camPos = owner.game.cameras[0].pos;
 		MoveSprite(1, absPos);
 		Data.handlePos = (subNodes[0] as Handle)!.pos;
 		FloatRect rect = Data.Rect;
 		rect.right++;
 		rect.top++;
 		MoveSprite(1, new Vector2(rect.left, rect.bottom) - camPos);
-		fSprites[1].scaleY = rect.Height() + 1f;
+		fsprs[1].scaleY = rect.Height() + 1f;
 		MoveSprite(2, new Vector2(rect.left, rect.bottom) - camPos);
-		fSprites[2].scaleX = rect.Width() + 1f;
+		fsprs[2].scaleX = rect.Width() + 1f;
 		MoveSprite(3, new Vector2(rect.right, rect.bottom) - camPos);
-		fSprites[3].scaleY = rect.Height() + 1f;
+		fsprs[3].scaleY = rect.Height() + 1f;
 		MoveSprite(4, new Vector2(rect.left, rect.top) - camPos);
-		fSprites[4].scaleX = rect.Width() + 1f;
+		fsprs[4].scaleX = rect.Width() + 1f;
 		MoveSprite(5, new Vector2(rect.left, rect.bottom) - camPos);
-		fSprites[5].scaleX = rect.Width() + 1f;
-		fSprites[5].scaleY = rect.Height() + 1f;
+		fsprs[5].scaleX = rect.Width() + 1f;
+		fsprs[5].scaleY = rect.Height() + 1f;
 	}
 }
 
 internal static class Extensions
 {
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static float Height(this FloatRect self) => Math.Abs(self.top - self.bottom);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static float Width(this FloatRect self) => Math.Abs(self.right - self.left);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static float Area(this FloatRect self) => self.Height() * self.Width();
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool InsideRect(Vector2 vec, FloatRect rect) => vec.x >= rect.left && vec.x <= rect.right && vec.y >= rect.bottom && vec.y <= rect.top;
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool InsideRect(float x, float y, FloatRect rect) => x >= rect.left && x <= rect.right && y >= rect.bottom && y <= rect.top;
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool EqualsFloatRect(this FloatRect self, FloatRect other) => self.left == other.left && self.right == other.right && self.bottom == other.bottom && self.top == other.top;
 
 	public static bool InsideNWSRects(this Player self)
 	{
 		if (self.room?.updateList is List<UpdatableAndDeletable> lst)
 		{
+			BodyChunk[] bs = self.bodyChunks;
 			for (var i = 0; i < lst.Count; i++)
 			{
 				if (lst[i] is NoWallSlideZone nws)
 				{
-					foreach (BodyChunk bc in self.bodyChunks)
+					for (var j = 0; j < bs.Length; j++)
 					{
-						if (InsideRect(bc.pos, nws._rect))
+						if (InsideRect(bs[j].pos, nws._rect))
 							return true;
 					}
 				}
--- src/Modules/Objects/_Module.cs
+++ src/Modules/Objects/_Module.cs
@@ -241,7 +241,7 @@ public static class _Module
 			if (pObj == null)
 			{
 				pObj = new PlacedObject(tp, null);
-				pObj.pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new Vector2(-683f, 384f), 0.25f) + Custom.DegToVec(UnityEngine.Random.value * 360f) * 0.2f;
+				pObj.pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new Vector2(-683f, 384f), 0.25f) + DegToVec(UnityEngine.Random.value * 360f) * 0.2f;
 				self.RoomSettings.placedObjects.Add(pObj);
 			}
 			DevInterface.PlacedObjectRepresentation placedObjectRepresentation;
@@ -259,7 +259,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + Custom.DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new LittlePlanetRepresentation(self.owner, "LittlePlanet_Rep", self, pObj);
@@ -272,7 +272,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new FloatRectRepresentation(self.owner, $"{tp}_Rep", self, pObj, tp.ToString());
@@ -285,7 +285,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new CESRepresentation(self.owner, $"{tp}_Rep", self, pObj);
@@ -298,7 +298,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new UpDownWFRepresentation(self.owner, $"{tp}_Rep", self, pObj, tp.ToString());
@@ -311,7 +311,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new ColoredLightBeamRepresentation(self.owner, $"{tp}_Rep", self, pObj);
@@ -324,7 +324,7 @@ public static class _Module
 			{
 				self.RoomSettings.placedObjects.Add(pObj = new(tp, null)
 				{
-					pos = self.owner.room.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
+					pos = self.owner.game.cameras[0].pos + Vector2.Lerp(self.owner.mousePos, new(-683f, 384f), .25f) + DegToVec(UnityEngine.Random.value * 360f) * .2f
 				});
 			}
 			var pObjRep = new ClimbJumpVineRepresentation(self.owner, $"{tp}_Rep", self, pObj);

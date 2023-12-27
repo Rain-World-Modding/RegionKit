ac6baf5f8ef007cebeed8e472e3ef8b6dec3fdc2
thalber <filefillman@gmail.com>
2023-12-27 13:31:05 +0400

    docstrings

--- src/API/Iggy.cs
+++ src/API/Iggy.cs
@@ -2,8 +2,16 @@ using Impl = RegionKit.Modules.Iggy;
 
 namespace RegionKit.API;
 
+/// <summary>
+/// Functions related to dev-iggy (devtools clippy in a hardhat)
+/// </summary>
 public static class Iggy
 {
+	/// <summary>
+	/// Gives a DevUINode a tooltip. Uses a callback to allow different text based on element state.
+	/// <para/>
+	/// If you are making a new UI node class, you should implement <see cref="global::RegionKit.Modules.Iggy.IGiveAToolTip"/> instead.
+	/// </summary>
 	public static void AddTooltip(DevInterface.DevUINode node, Func<Impl.ToolTip> toolTipProducer)
 	{
 		ThrowIfModNotInitialized();
--- src/Modules/Iggy/ToolTip.cs
+++ src/Modules/Iggy/ToolTip.cs
@@ -1,3 +1,10 @@
 namespace RegionKit.Modules.Iggy;
 
+/// <summary>
+/// Represents an iggy tooltip that appears on right clicking a devtools UI element.
+/// </summary>
+/// <param name="text">What Iggy should say.</param>
+/// <param name="priority">Value that determines how important tooltip from a particular element is. Highest value element is chosen. Big container-like elements such as Panels have 5, buttons have 10, Iggy itself has 1.</param>
+/// <param name="source">Node that gave the tooltip. Used to prevent duplicates when requesting a tooltip.</param>
+/// <returns></returns>
 public record ToolTip(string text, int priority, DevInterface.DevUINode source);

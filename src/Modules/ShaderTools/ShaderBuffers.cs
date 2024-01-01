cb144e1f4198ce0c75fcfd9203235231ed225ca4
Eti // Xan <subzeroxan@gmail.com>
2024-01-01 14:33:58 -0600

    Update ShaderBuffers.cs

--- src/Modules/ShaderTools/ShaderBuffers.cs
+++ src/Modules/ShaderTools/ShaderBuffers.cs
@@ -8,13 +8,17 @@ namespace RegionKit.Modules.ShaderTools {
 		/// The amount of bits needed to activate the stencil buffer.
 		/// </summary>
 		private const int DEPTH_AND_STENCIL_BUFFER_BITS = 24;
+		
+		private static bool _hasStencilBuffer = false;
+		private static bool _initialized = false;
 
 		/// <summary>
 		/// Requests that the stencil buffer is enabled, setting the depth bit count of the main screen's render texture to 24 bits.
-    /// Once at least one mod calls this, it will be permanently enabled and 
+		/// Once at least one mod calls this, it will be permanently enabled and 
 		/// </summary>
 		public static void RequestStencilBuffer() {
-			if (Futile.screen != null) {
+			if (!_hasStencilBuffer && Futile.screen != null) {
+				_hasStencilBuffer = true;
 				RenderTexture rt = Futile.screen.renderTexture;
 				if (rt.depth < DEPTH_AND_STENCIL_BUFFER_BITS) {
 					rt.depth = DEPTH_AND_STENCIL_BUFFER_BITS;
@@ -23,18 +27,20 @@ namespace RegionKit.Modules.ShaderTools {
 		}
 
 		internal static void Initialize() {
+			if (_initialized) return;
+			_initialized = true;
 			On.FScreen.ctor += OnConstructingFScreen;
 			On.FScreen.ReinitRenderTexture += OnReinitializeRT;
 		}
-
+		
 		private static void OnReinitializeRT(On.FScreen.orig_ReinitRenderTexture originalMethod, FScreen @this, int displayWidth) {
 			originalMethod(@this, displayWidth);
-			@this.renderTexture.depth = _highestRequestedBits;
+			@this.renderTexture.depth = _hasStencilBuffer ? DEPTH_AND_STENCIL_BUFFER_BITS : @this.renderTexture.depth;
 		}
 
 		private static void OnConstructingFScreen(On.FScreen.orig_ctor originalCtor, FScreen @this, FutileParams futileParams) {
 			originalCtor(@this, futileParams);
-			@this.renderTexture.depth = _highestRequestedBits;
+			@this.renderTexture.depth = _hasStencilBuffer ? DEPTH_AND_STENCIL_BUFFER_BITS : @this.renderTexture.depth;
 		}
 
 	}

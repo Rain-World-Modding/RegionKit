297e42f8e2afe77d2788e2be1297125979f65a06
Eti // Xan <subzeroxan@gmail.com>
2024-01-01 14:37:07 -0600

    Create _Module.cs

---
+++ src/Modules/ShaderTools/_Module.cs
@@ -0,0 +1,24 @@
+﻿namespace RegionKit.Modules.ShaderTools {
+	[RegionKitModule(nameof(Enable), nameof(Disable), moduleName: "Shader Tools")]
+	public static class _Module {
+		/// <summary>
+		/// Applies hooks.
+		/// </summary>
+		public static void Enable() {
+			ShaderBuffers.Initialize();
+		}
+
+		/// <summary>
+		/// Undoes hooks.
+		/// </summary>
+		public static void Disable() {
+			// This probably should *not* be implemented.
+			// Editing the RT's bits has no adverse effects:
+			// 1. Depth doesn't work because *Unity* only renders the game's RT, and nothing else.
+			// 2. The stencil buffer is never set by any native shaders, meaning it is all 0 anyway by default.
+			
+			// And in exchange, disabling the buffer might brick compatibility with other mods that 
+			// enable it on their own, by unexpectedly disabling it when they might be trying to use it.
+		}
+	}
+}

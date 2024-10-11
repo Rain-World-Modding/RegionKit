e7741a7ea10b934654a09139a8ece6da62a084fd
Bro <bro479508@gmail.com>
2024-10-10 21:59:48 -0600

    Construction Lock & Tat Art
    Adds the 'construction' lock which is automatically set if the next region doesn't exist. Also some gate art by Tat - 1-5 is used with 'txt' and their special lock art is used with 'alt'

--- src/Modules/Misc/_Enums.cs
+++ src/Modules/Misc/_Enums.cs
@@ -1,4 +1,4 @@
-//extended gates by Henpemaz
+﻿//extended gates by Henpemaz
 
 using Req = RegionGate.GateRequirement;
 
@@ -6,6 +6,7 @@ namespace RegionKit.Modules.Misc;
 
 public static class _Enums
 {
+	public static Req Construction = new(nameof(Construction), true);
 	public static Req uwu = new(nameof(uwu), true);
 	public static Req Open = new(nameof(Open), true);
 	public static Req Forbidden = new(nameof(Forbidden), true);
@@ -17,12 +18,30 @@ public static class _Enums
 	public static Req EightKarma = new("8", true);
 	public static Req NineKarma = new("9", true);
 	public static Req TenKarma = new("10", true);
-	public static Req[] alt = new Req[10];
+	public static Req[] alt = new Req[17];
+	public static Req[] txt = new Req[5];
 	public static void Register()
 	{
-		foreach (int i in Range(10))
+		const int numericals = 10;
+		Req[] specials = new Req[7] { Construction, uwu, Open, Forbidden, Glow, CommsMark, TenReinforced };
+
+		alt = new Req[numericals + specials.Length];
+
+		foreach (int i in Range(5))
+		{
+			txt[i] = new((i + 1) + ExtendedGates.TXT_POSTFIX, true);
+		}
+
+		foreach (int i in Range(numericals))
 		{
 			alt[i] = new((i + 1) + ExtendedGates.ALT_POSTFIX, true);
+		}
+
+
+		for (int i = 0; i < specials.Length; i++)
+		{
+			LogInfo("ExtendedGates registering special " + specials[i].value + ExtendedGates.ALT_POSTFIX);
+			alt[i + numericals] = new(specials[i].value + ExtendedGates.ALT_POSTFIX, true);
 		}
 	}
 }

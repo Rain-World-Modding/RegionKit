e595ea7433fe1a00e67739c4bdf04f06d2ee4ec9
MagicaJaphet <Linda.Shutter@gmail.com>
2025-09-28 00:08:53 -0400

    Reworked replace corruption color to have DLL color persist across rooms when the DLL gets abstracted. Fixed ColoredSnowSource and added a single null check.

--- src/Modules/MultiColorSnow/ColoredSnowRoomCamera.cs
+++ src/Modules/MultiColorSnow/ColoredSnowRoomCamera.cs
@@ -41,111 +41,114 @@ public class ColoredSnowRoomCamera
 	}
 
 	private static readonly PropertyInfo _RoomCamera_fadeCoord = typeof(RoomCamera).GetProperty("fadeCoord", BindingFlags.NonPublic | BindingFlags.Instance);
-	private static readonly PropertyInfo _RoomCamera_levelTexture = typeof(RoomCamera).GetProperty("levelTexture", BindingFlags.NonPublic | BindingFlags.Instance);
+	private static readonly PropertyInfo _RoomCamera_levelTexture = typeof(RoomCamera).GetProperty("levelTexture", BindingFlags.Public | BindingFlags.Instance);
 
 	public static void UpdateSnowLight(RoomCamera camera)
 	{
 		ColoredSnowRoomCamera cameraData = ColoredSnowRoomCamera.GetData(camera);
 		ColoredSnowWeakRoomData roomData = ColoredSnowWeakRoomData.GetData(camera.room);
 
-		cameraData.palette ??= (Color[])empty3.Clone();
-
-		int source = 0;
-
-		float[] shapeBuffer = new float[4];
-		Vector2[] depthBuffer = new Vector2[2];
-		float[] paletteBuffer = new float[4];
-
-		Color[] packedSources = (Color[])empty.Clone();
-		Color[] packedSources2 = (Color[])empty2.Clone();
-
-		for (int i = 0; i < roomData.snowSources.Count && source < 20; i++)
+		if (cameraData.coloredSnowPalette != null)
 		{
-			ColoredSnowSourceUAD snowSource = roomData.snowSources[i];
+			cameraData.palette ??= (Color[])empty3.Clone();
 
-			if (snowSource.visibility == 1)
+			int source = 0;
+
+			float[] shapeBuffer = new float[4];
+			Vector2[] depthBuffer = new Vector2[2];
+			float[] paletteBuffer = new float[4];
+
+			Color[] packedSources = (Color[])empty.Clone();
+			Color[] packedSources2 = (Color[])empty2.Clone();
+
+			for (int i = 0; i < roomData.snowSources.Count && source < 20; i++)
 			{
-				Vector4[] packedData = snowSource.PackSnowData();
-				packedSources[source] = new Color(packedData[0].x, packedData[0].y, packedData[0].z, packedData[0].w);
-				packedSources[source + 20] = new Color(packedData[1].x, packedData[1].y, packedData[1].z, packedData[1].w);
-				shapeBuffer[source % 4] = packedData[2].w;
-				paletteBuffer[source % 4] = (float)snowSource.data.palette / 255.0F;
+				ColoredSnowSourceUAD snowSource = roomData.snowSources[i];
 
-				if ((source + 1) % 4 == 0)
+				if (snowSource.visibility == 1)
 				{
-					packedSources[source / 4 + 40] = new Color(shapeBuffer[0], shapeBuffer[1], shapeBuffer[2], shapeBuffer[3]);
-					packedSources2[source / 4 + 10] = new Color(paletteBuffer[0], paletteBuffer[1], paletteBuffer[2], paletteBuffer[3]);
+					Vector4[] packedData = snowSource.PackSnowData();
+					packedSources[source] = new Color(packedData[0].x, packedData[0].y, packedData[0].z, packedData[0].w);
+					packedSources[source + 20] = new Color(packedData[1].x, packedData[1].y, packedData[1].z, packedData[1].w);
+					shapeBuffer[source % 4] = packedData[2].w;
+					paletteBuffer[source % 4] = (float)snowSource.data.palette / 255.0F;
+
+					if ((source + 1) % 4 == 0)
+					{
+						packedSources[source / 4 + 40] = new Color(shapeBuffer[0], shapeBuffer[1], shapeBuffer[2], shapeBuffer[3]);
+						packedSources2[source / 4 + 10] = new Color(paletteBuffer[0], paletteBuffer[1], paletteBuffer[2], paletteBuffer[3]);
+					}
+
+					if (roomData.snowPalettes.ContainsKey(snowSource.data.palette))
+					{
+						cameraData.palette[snowSource.data.palette] = roomData.snowPalettes[snowSource.data.palette].getBlendedRGBA(((Vector4)_RoomCamera_fadeCoord.GetValue(camera)).y);
+						depthBuffer[source % 2] = new Vector2((float)roomData.snowPalettes[snowSource.data.palette].data.from / 30f, (float)roomData.snowPalettes[snowSource.data.palette].data.to / 30f);
+					}
+					else
+					{
+						cameraData.palette[snowSource.data.palette] = Color.white;
+						depthBuffer[source % 2] = new Vector2(0, 1);
+					}
+
+					if ((source + 1) % 2 == 0)
+					{
+						packedSources2[source / 2] = new Color(depthBuffer[0].x, depthBuffer[0].y, depthBuffer[1].x, depthBuffer[1].y);
+					}
+
+					source++;
 				}
 
-				if (roomData.snowPalettes.ContainsKey(snowSource.data.palette))
+			}
+
+			if (source > 0)
+			{
+				source--;
+
+				if ((source + 1) % 4 != 0)
 				{
-					cameraData.palette[snowSource.data.palette] = roomData.snowPalettes[snowSource.data.palette].getBlendedRGBA(((Vector4)_RoomCamera_fadeCoord.GetValue(camera)).y);
-					depthBuffer[source % 2] = new Vector2((float)roomData.snowPalettes[snowSource.data.palette].data.from / 30f, (float)roomData.snowPalettes[snowSource.data.palette].data.to / 30f);
-				}
-				else
-				{
-					cameraData.palette[snowSource.data.palette] = Color.white;
-					depthBuffer[source % 2] = new Vector2(0, 1);
+					int s = source / 4 + 40;
+					int s2 = source / 4 + 10;
+					switch ((source + 1) % 4)
+					{
+					case 1:
+						packedSources[s] = new Color(shapeBuffer[0], 0, 0, 0);
+						packedSources2[s2] = new Color(paletteBuffer[0], 0, 0, 0);
+						break;
+					case 2:
+						packedSources[s] = new Color(shapeBuffer[0], shapeBuffer[1], 0, 0);
+						packedSources2[s2] = new Color(paletteBuffer[0], paletteBuffer[1], 0, 0);
+						break;
+					case 3:
+						packedSources[s] = new Color(shapeBuffer[0], shapeBuffer[1], shapeBuffer[2], 0);
+						packedSources2[s2] = new Color(paletteBuffer[0], paletteBuffer[1], paletteBuffer[2], 0);
+						break;
+					}
 				}
 
-				if ((source + 1) % 2 == 0)
+				if ((source + 1) % 2 != 0)
 				{
-					packedSources2[source / 2] = new Color(depthBuffer[0].x, depthBuffer[0].y, depthBuffer[1].x, depthBuffer[1].y);
+					packedSources2[source / 2] = new Color(depthBuffer[0].x, depthBuffer[0].y, 0, 0);
 				}
 
+				packedSources2[15] = new Color((source / 20.0F), 0, 0, 0);
 				source++;
+				Shader.EnableKeyword("SNOW_ON");
 			}
-
-		}
-
-		if (source > 0)
-		{
-			source--;
-
-			if ((source + 1) % 4 != 0)
+			else
 			{
-				int s = source / 4 + 40;
-				int s2 = source / 4 + 10;
-				switch ((source + 1) % 4)
-				{
-				case 1:
-					packedSources[s] = new Color(shapeBuffer[0], 0, 0, 0);
-					packedSources2[s2] = new Color(paletteBuffer[0], 0, 0, 0);
-					break;
-				case 2:
-					packedSources[s] = new Color(shapeBuffer[0], shapeBuffer[1], 0, 0);
-					packedSources2[s2] = new Color(paletteBuffer[0], paletteBuffer[1], 0, 0);
-					break;
-				case 3:
-					packedSources[s] = new Color(shapeBuffer[0], shapeBuffer[1], shapeBuffer[2], 0);
-					packedSources2[s2] = new Color(paletteBuffer[0], paletteBuffer[1], paletteBuffer[2], 0);
-					break;
-				}
+				Shader.DisableKeyword("SNOW_ON");
 			}
 
-			if ((source + 1) % 2 != 0)
-			{
-				packedSources2[source / 2] = new Color(depthBuffer[0].x, depthBuffer[0].y, 0, 0);
-			}
-
-			packedSources2[15] = new Color((source / 20.0F), 0, 0, 0);
-			source++;
-			Shader.EnableKeyword("SNOW_ON");
+			roomData.snowObject!.visibleSnow = source;
+			cameraData.coloredSnowSources.SetPixels(packedSources);
+			cameraData.coloredSnowSources.Apply();
+			cameraData.coloredSnowSources2.SetPixels(packedSources2);
+			cameraData.coloredSnowSources2.Apply();
+			cameraData.coloredSnowPalette.SetPixels(cameraData.palette);
+			cameraData.coloredSnowPalette.Apply();
+			Graphics.Blit((Texture2D)_RoomCamera_levelTexture.GetValue(camera), cameraData.coloredSnowTexture, _Module.RKLevelSnowMaterial);
+			cameraData.snowChange = false;
 		}
-		else
-		{
-			Shader.DisableKeyword("SNOW_ON");
-		}
-
-		roomData.snowObject!.visibleSnow = source;
-		cameraData.coloredSnowSources.SetPixels(packedSources);
-		cameraData.coloredSnowSources.Apply();
-		cameraData.coloredSnowSources2.SetPixels(packedSources2);
-		cameraData.coloredSnowSources2.Apply();
-		cameraData.coloredSnowPalette.SetPixels(cameraData.palette);
-		cameraData.coloredSnowPalette.Apply();
-		Graphics.Blit((Texture2D)_RoomCamera_levelTexture.GetValue(camera), cameraData.coloredSnowTexture, _Module.RKLevelSnowMaterial);
-		cameraData.snowChange = false;
 	}
 
 	public static ColoredSnowRoomCamera GetData(RoomCamera obj)

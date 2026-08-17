2ccd5b90969b0f9fc92b6ed25b06a4896e3e2940
alduris <152772325+alduris@users.noreply.github.com>
2026-08-17 05:11:16 -0400

    Swap out SharpVoronoiLib with NuGet package

--- src/Modules/Misc/SharpVoronoiLib/Border Clipping/IBorderClippingAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SharpVoronoiLib
-{
-    internal interface IBorderClippingAlgorithm
-    {
-        List<VoronoiEdge> Clip(List<VoronoiEdge> edges, double minX, double minY, double maxX, double maxY);
-    }
-}
--- src/Modules/Misc/SharpVoronoiLib/Tessellation/ITessellationAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SharpVoronoiLib
-{
-    internal interface ITessellationAlgorithm
-    {
-        List<VoronoiEdge> Run(List<VoronoiSite> sites, double minX, double minY, double maxX, double maxY);
-    }
-}
